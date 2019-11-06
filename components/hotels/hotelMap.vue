9545668544fc6becf76c59fdaa3364f2d2e08187
乐 <836607178@qq.com>
2019-11-06 19:24:25 +0800

    酒店详情页

--- components/hotels/hotelMap.vue
+++ components/hotels/hotelMap.vue
@@ -11,18 +11,32 @@ export default {
       // eslint-disable-next-line no-undef
       const map = new AMap.Map('container', {
         zoom: 12,
-        center: [116.39, 39.9]
-
+        // 初始化以这个为中心
+        center: [113.27591426455703, 23.11620128356685]
       })
-      console.log(map)
+      // eslint-disable-next-line no-undef
+      const toolbar = new AMap.ToolBar()
+      map.addControl(toolbar)
+      // 创建一个点标记
+      // 创建一个 Marker 实例：
+      // eslint-disable-next-line no-undef
+
+      // eslint-disable-next-line no-undef
+      const marker = new AMap.Marker({
+        // eslint-disable-next-line no-undef
+        position: new AMap.LngLat(113.27591426455703, 23.11620128356685), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+        title: '越秀'
+      })
+      // 将创建的点标记添加到已有的地图实例：
+      map.add(marker)
+      // 自定义点标记
     }
 
-    const url = 'https://webapi.amap.com/maps?v=1.4.15&key=ede0e3db76feda1de3a439a35962ae2c&callback=onLoad'
+    const url = 'https://webapi.amap.com/maps?v=1.4.15&key=ede0e3db76feda1de3a439a35962ae2c&callback=onLoad&plugin=AMap.ToolBar'
     const jsapi = document.createElement('script')
     jsapi.charset = 'utf-8'
     jsapi.src = url
     document.head.appendChild(jsapi)
-
     // 使用插件
   }
 
